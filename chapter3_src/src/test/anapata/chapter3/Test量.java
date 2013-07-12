2db959b01226c35692c0f318c5ac6b1c358703dd
bn-kyo <bn-kyo@canon-electec.co.jp>
2013-07-12 16:43:17 +0900

    ファイル文字コード修正.

---
+++ chapter3_src/src/test/anapata/chapter3/Test量.java
@@ -0,0 +1,56 @@
+package test.anapata.chapter3;
+
+import anapata.chapter3.単位;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import anapata.chapter3.量;
+
+import static org.junit.Assert.assertEquals;
+
+public class Test量 {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test単位の変換_サポート外の単位() {
+		量 feet2 = new 量(2, anapata.chapter3.単位.FEET);
+		try{
+			feet2.convertTo(anapata.chapter3.単位.CM);
+		}catch(RuntimeException ex){
+			assertEquals("not supported", ex.getMessage());
+		}
+	}
+	@Test
+	public void test単位の変換_正() {
+		量 feet2 = new 量(2, anapata.chapter3.単位.FEET);
+		assertEquals(new 量(24, anapata.chapter3.単位.INCH), feet2.convertTo(anapata.chapter3.単位.INCH));
+	}
+	@Test
+	public void test単位の変換_逆() {
+		量 inch12 = new 量(12, anapata.chapter3.単位.INCH);
+		assertEquals(new 量(1, anapata.chapter3.単位.FEET), inch12.convertTo(anapata.chapter3.単位.FEET));
+	}
+	@Test
+	public void test単位の変換_ホップ() {
+		量 mm1000 = new 量(1000, anapata.chapter3.単位.MM);
+		assertEquals(new 量(1, anapata.chapter3.単位.M), mm1000.convertTo(単位.M));
+	}
+}
